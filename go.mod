e05fb8ca202dd52c62d696fbeca057e26353d066
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-11-01 00:04:38 +0700

    update say hello function with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/anggyar/go-say-hello
+module github.com/anggyar/go-say-hello/v2
 
 go 1.24.5
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello, World! Ini ada update terbaru"
+func SayHello(name string) string {
+	return "Hello bang " + name
 }
